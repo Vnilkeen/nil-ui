c85e9095edd0b5ad7d1baf68f73d81411e68802a
vnilkeen <vnilkeen@outlook.com>
2025-05-25 23:05:40 +0400

    refactor: remove global styles and layout components, migrate to SCSS for styling
    - Deleted globals.css, layout.js, page.js, and page.module.css.
    - Introduced new layout.jsx and page.jsx with updated structure.
    - Added SCSS files for variables and elements styling.
    - Implemented new button styles and typography using SCSS mixins.
    - Updated package.json to include sass as a dependency.
    - Adjusted jsconfig.json to reflect new path structure.

---
+++ app/page.jsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main>
+      <div className="button-accent button">Button</div>
+      <div className="button-white button">Button</div>
+      <div className="button-black button">Button</div>
+    </main>    
+  );
+}
